f32dda486a4644eab115409fa33bab43dc338ac3
SilvieVop <159158065+SilvieVop@users.noreply.github.com>
2024-02-08 17:08:24 +0100

    Add files via upload
    upraveno stejne jako soubor tri_musketyr. nutne dodelat kouzla a mozna zmenit i styl boje. Stejná chyba všichni bojují se všema a všichni umřou

--- carodejove.py
+++ carodejove.py
@@ -1,10 +1,9 @@
+# Změnit styl boje s kouzli - zmrazení, ohěň, atd co by šlo použít??
+
+
 import random
 
 from colorama import Fore, Style
-
-import bonus_zbrane # zde jsem si myslela, že použijí zbran pouze v případě bonusu.. dá se ještě předělat
-
-import zakladni_vstupy
 
 
 class Carodejove():
@@ -36,14 +35,13 @@ class Carodejove():
         assert isinstance(kouzlo, object)
         return kouzlo
 
-    @property
-    def zautoc(self):
-        if bonus_zbrane: True
-        utok = random.randint(1,self.utok)
-        print(f"{self.jmeno} zaútočil na {self.vybrany_carodej} se silou útoku: {utok}.")
+    @property # getter
+    def zautoc(self):  # Dekorátor ?? kam dát setter a co do něj?? tady chci aby mi to generoval jméno protivnika -> nefunguje
+        utok = random.randint(1, self.utok)
+        print(f"{self.jmeno} zaútočil na {vybrany_carodej} se silou útoku: {utok}.")
         return utok
 
-    def bran_se(self):
+    def bran_se(self, utok):  # tohle bude platit pro oba pri hre
         obrana = random.randint(1, self.obrana)
         print(f"{self.jmeno} se brání silou: {obrana}.\n")
         return obrana
@@ -53,53 +51,48 @@ class Carodejove():
         self.zivot -= poraneni
         return poraneni
 
-    def aktualni_zivot(self, poraneni=None): # tohle by melo ukazovat, kolik bodů poraneni obdrzel a kolik zbyva zivotů pred smrti
+    def aktualni_zivot(self):
         zivot = self.zivot + self.obrana - self.utok
-        if zivot > 0:
-            print(f"{self.jmeno} utrpěl {poraneni} bodů poškození a jeho zbývající život je {zivot} bodů.")
-            # print(f"Zbývající život je {self.zivot} bodů.")
+        poraneni = self.utok - self.obrana  # Vypočítat poškození bez použití max()
+        self.zivot -= max(poraneni,1)  # Snížit život o poškození (alespoň 0)
 
-        elif zivot <= 0:
-            print(f"{self.jmeno} si připsal {poraneni} bodů ke svému životu")
-            return zivot
-
+        if self.zivot > 0:
+            print(f"{self.jmeno} utrpěl {max(poraneni,0)} bodů poškození a jeho zbývající život je {self.zivot} bodů.")
         else:
             self.zivot = 0
             print(Fore.RED + f"{self.jmeno} JE MRTVÝ" + Style.RESET_ALL)
 
-vybrany_carodej = "{vybrany_carodej}" # tak dobrý.. už mi to generuje vybraneho musketyra ale jak mu nadefinuji aby to vzal z listu zakladni_vstupy??
+        return self.zivot
+
+vybrany_carodej = "Protivnik"
 
 class Carodej(Carodejove):
-    def __init__(self, jmeno, utok, zraneni, obrana, kouzla):
-        super(). __init__(jmeno, 100, utok, zraneni, obrana, kouzla)
+    def __init__(self, jmeno, utok, zraneni, obrana, mana, vybrany_carodej):
+        super().__init__(jmeno, 10, utok, zraneni, obrana, mana, vybrany_carodej)
 
-carodej1 = Carodejove("Gandalf", 21, 32, 24, 10, 10, [vybrany_carodej])
-carodej1.kouzli(10)
-carodej1.zautoc()
-carodej1.bran_se()
-carodej1.aktualni_zivot()
+carodej1 = Carodej("Gandalf", 2, 3, 1, 6, vybrany_carodej)
+carodej2 = Carodej("Merlin", 2, 3, 2, 5, vybrany_carodej)
+carodej3 = Carodej("Kaprfíld", 2, 2, 2, 5, vybrany_carodej)
+carodej4 = Carodej("Žito", 3, 2, 2, 5, vybrany_carodej)
 
-carodej2 = Carodejove("Merlin", 20, 31, 33, 10, 10, [vybrany_carodej])
-carodej2.kouzli(10)
-carodej2.zautoc()
-carodej2.bran_se()
-carodej2.aktualni_zivot()
-
-carodej3 = Carodejove("Kaprfíld", 20, 23, 26, 10, 10,[vybrany_carodej])
-carodej3.kouzli(10)
-carodej3.zautoc()
-carodej3.bran_se()
-carodej3.aktualni_zivot()
-
-carodej4 = Carodejove("Žito", 19, 32, 29, 10, 10, [vybrany_carodej])
-carodej4.kouzli(10)
-carodej4.zautoc()
-carodej4.bran_se()
-carodej4.aktualni_zivot()
-
-# TODO zapojit vybrnyho protivnika čarodeje!! zatim nefunguje
-vybrany_carodej = Carodejove(jmeno=[vybrany_carodej])
-vybrany_carodej.kouzli(10)
-vybrany_carodej.zautoc()
-vybrany_carodej.bran_se()
-vybrany_carodej.aktualni_zivot()
+while carodej1.zivot > 0 or carodej2.zivot > 0 or carodej3.zivot > 0 or carodej4.zivot > 0:
+    if carodej1.zivot > 0:
+        carodej1.bran_se(1)
+        carodej1.zautoc
+        carodej1.kouzli(mana=1)
+        carodej1.aktualni_zivot()
+    if carodej2.zivot > 0:
+        carodej2.bran_se(1)
+        carodej2.zautoc
+        carodej2.kouzli(mana=1)
+        carodej2.aktualni_zivot()
+    if carodej3.zivot > 0:
+        carodej3.bran_se(1)
+        carodej3.zautoc
+        carodej3.kouzli(mana=1)
+        carodej3.aktualni_zivot()
+    if carodej4.zivot > 0:
+        carodej4.bran_se(1)
+        carodej4.kouzli(mana=1)
+        carodej4.zautoc
+        carodej4.aktualni_zivot()
